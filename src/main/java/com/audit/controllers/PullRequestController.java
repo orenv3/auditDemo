67d577cc797f5074ba28dd6ffc55bd2eff9a1e9c
orenv3 <vinogura.oren@gmail.com>
2022-10-27 16:16:20 +0300

    adding @CrossOrigin to the controller

--- src/main/java/com/audit/controllers/PullRequestController.java
+++ src/main/java/com/audit/controllers/PullRequestController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import com.audit.services.PullRequestSrvInterface;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@CrossOrigin
 @RequestMapping("api/pullRequest")
 @RestController
 public class PullRequestController {
