77bddfcdc799aae095b61e68036614fef3282ad9
orenv3 <vinogura.oren@gmail.com>
2022-10-29 15:45:42 +0300

    change the pic path to tomcat

--- src/main/java/com/audit/printscreen/PullRequestScreenshot.java
+++ src/main/java/com/audit/printscreen/PullRequestScreenshot.java
@@ -9,15 +9,16 @@ public class PullRequestScreenshot extends PrintScreen{
 	
 	public PullRequestScreenshot(PullRequestWarraper prWarraer) {
 		this.setName(this.getClass().getSimpleName());
-		this.BASE_DIRECTOEY_PATH="C:\\pullRequest_screenshots";
-		this.url = prWarraer.getPull_request().getHtml_url();
-		
 		setNameFormat(prWarraer.getPull_request().getCreated_at()
 				,"ddMMyy",prWarraer.getPull_request().getUserName()
 				, prWarraer.getAction()
 				,prWarraer.getPull_request().getPullRequest_id()+"");
 		this.picture = new Picture(this.strBuilder.toString());
+		
+		this.BASE_DIRECTOEY_PATH="C:\\Program Files\\Apache Software Foundation\\Tomcat 9.0\\webapps\\myApp\\pullRequest_screenshots";
 		createFilePath(BASE_DIRECTOEY_PATH);
+//		this.BASE_DIRECTOEY_PATH="C:\\pullRequest_screenshots";
+		this.url = prWarraer.getPull_request().getHtml_url();
 	}
 	
 	
